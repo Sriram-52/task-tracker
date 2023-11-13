7098ada8a9aab838703a82aef99e7fad822e18e3
Sriram Nyshadham <nyshadhamramu@gmail.com>
2023-11-12 22:01:08 -0500

    add mui

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@assets/scss/custom.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</React.StrictMode>
---
+++ apps/client/src/theme.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+	Link as RouterLink,
+	LinkProps as RouterLinkProps,
+} from "react-router-dom";
+import { LinkProps } from "@mui/material/Link";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/700.css";
+import { createTheme } from "@mui/material";
+
+const LinkBehavior = React.forwardRef<
+	HTMLAnchorElement,
+	Omit<RouterLinkProps, "to"> & { href: RouterLinkProps["to"] }
+>((props, ref) => {
+	const { href, ...other } = props;
+	// Map href (Material UI) -> to (react-router)
+	return <RouterLink ref={ref} to={href} {...other} />;
+});
+
+export const theme = createTheme({
+	components: {
+		MuiLink: {
+			defaultProps: {
+				component: LinkBehavior,
+			} as LinkProps,
+		},
+		MuiButtonBase: {
+			defaultProps: {
+				LinkComponent: LinkBehavior,
+			},
+		},
+	},
+});
